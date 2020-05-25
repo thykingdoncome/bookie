b353293821f74998657892a295db35846bfac761
israel david <davidisrael194@gmail.com>
2020-05-25 16:36:00 +0100

    sweet alert

--- src/components/Container.vue
+++ src/components/Container.vue
@@ -1,20 +1,35 @@
 <template>
   <div class="page-container ma-0">
-   <Header />
+    <Header />
 
-   <div class="form py-1">
+    <div class="form py-1">
       <form id="form" @submit.prevent="fetchBooks">
-     <div class="fields">
-        <input type="text" class="mr-2" id="query" v-model="query" placeholder="Search by title keyword" autofocus/>
-      <input type="text" class="mr-2" id="author" v-model="authorName" placeholder="Search by Author"/>
-     </div>
-     <div class="search"> <input type="submit" value="Search"></div>
-    </form>
-   </div>
+        <div class="fields">
+          <input
+            type="text"
+            class="mr-2"
+            id="query"
+            v-model="query"
+            placeholder="Search by title keyword"
+            autofocus
+          />
+          <input
+            type="text"
+            class="mr-2"
+            id="author"
+            v-model="authorName"
+            placeholder="Search by Author"
+          />
+        </div>
+        <div class="search"><input type="submit" value="Search" /></div>
+      </form>
+    </div>
 
     <div class="book-container">
-      <book v-for="book in this.books" :key="book.id" 
-       :bookCategory="book.volumeInfo.categories && book.volumeInfo.categories"
+      <book
+        v-for="book in this.books"
+        :key="book.id"
+        :bookCategory="book.volumeInfo.categories && book.volumeInfo.categories"
         :imgSrc="book.volumeInfo.imageLinks && book.volumeInfo.imageLinks.thumbnail"
         :bookAuthor="book.volumeInfo.authors && book.volumeInfo.authors"
         :bookTitle="book.volumeInfo.title && book.volumeInfo.title"
@@ -26,7 +41,6 @@
         :description="book.volumeInfo.description && book.volumeInfo.description"
       />
     </div>
-
   </div>
 </template>
 
@@ -43,7 +57,7 @@ export default {
   name: "Container",
   components: {
     Book,
-    Header
+    Header,
   },
   data() {
     return {
@@ -60,13 +74,24 @@ export default {
           `${VUE_APP_BASE_URL}?q=${this.query}+inauthor:${this.authorName}&key=${VUE_APP_API_KEY}&maxResults=40`
         );
         this.books = res.data.items;
+        if (this.books === undefined) {
+          this.$swal({
+            position: "top-end",
+            icon: "error",
+            html: `<span style="color:#990000">No volumes found for your search!</span>`,
+            showConfirmButton: false,
+            timer: 3500,
+            background: "rgba(238, 229, 229)",
+            toast: true,
+          });
+        }
         this.query = "";
         this.authorName = "";
       } catch (error) {
         console.log(error);
       }
     },
-  }
+  },
 };
 </script>
 
--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueSweetalert2 from 'vue-sweetalert2';
+
+Vue.use(VueSweetalert2);
 
 Vue.config.productionTip = false
 
